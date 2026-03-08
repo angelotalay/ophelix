684508ff51e0aa52a8d7a275b0860a232be94190
Angelo Talay <angelogabrieltalay@gmail.com>
2026-03-08 11:47:57 +0000

    Update WhyPage.tsx

--- features/marketing/pages/WhyPage.tsx
+++ features/marketing/pages/WhyPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import MarketingPageTemplate from "@/features/marketing/templates/MarketingPageTemplate";
 import HeadlineBlock from "@/features/marketing/components/headlineBlock/HeadlineBlock";
+import CarouselHeadline from "@/features/marketing/components/carouselHeadline/CarouselHeadline";
 import HERO_CONTENT from "@/features/marketing/pages/whyPage.copy";
 import HeadlineBlockCopy from "@/features/marketing/components/headlineBlock/headlineBlock.copy";
 
@@ -24,6 +25,7 @@ function WhyPage() {
         text={HeadlineBlockCopy.HEADLINE_BLOCK_CONTENT_2.text}
         intent={"extra-dark"}
       />
+      <CarouselHeadline />
     </MarketingPageTemplate>
   );
 }
