f32733c20152aba53193aa0e09bccd8c6c100c33
Angelo Talay <angelogabrieltalay@gmail.com>
2026-04-02 11:54:04 +0100

    Removed unused function and updated sanity urlfor function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function constructMuxUrl(playbackId: string): string {
-  console.log(`https://stream.mux.com/${playbackId}.m3u8`);
-  return `https://stream.mux.com/${playbackId}.m3u8`;
-}
